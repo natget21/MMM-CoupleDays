743ae68efca96eaf9eb7e6a278418e0fbf115852
Natnael Getachew <natget21@gmail.com>
2025-04-06 23:38:49 +0200

    add extraDates and image view option

--- MMM-CoupleDays.js
+++ MMM-CoupleDays.js
@@ -3,6 +3,12 @@ Module.register("MMM-CoupleDays", {
     name1: "Partner 1",
     name2: "Partner 2",
     date: "2023-01-01",
+    extraDates: [{label: "Since first event", date: "2023-01-02"}, {label: "Since second event", date: "2023-01-03"}],
+    showExtraDates: false,
+    ExtraDatesViewMode: "loop",
+    vectorImgUrl: "",
+    showVectorImg: false,
+    landscapeMode: false,
     transitionDuration: 7500,
     language: "en",
     textColor: "white"
@@ -26,15 +32,51 @@ Module.register("MMM-CoupleDays", {
     this.wrapper.className = "couple-days-wrapper";
     this.wrapper.style.color = this.config.textColor;
     this.wrapper.appendChild(this.headerWrapper);
+
+    if (this.config.showVectorImg) {
+      this.imageContentWrapper = document.createElement("div");
+      this.imageContentWrapper.className = "couple-days-image";
+
+      const img = document.createElement("img");
+      img.src = this.config.vectorImgUrl || "/modules/MMM-CoupleDays/assets/default/default-asset.png";
+      img.alt = "Couple Image";
+      img.className = "couple-image";
+      this.imageContentWrapper.appendChild(img);
+
+      if (this.config.landscapeMode) {
+        this.wrapper.style.display = "flex";
+        this.wrapper.style.alignItems = "center";
+        this.wrapper.appendChild(this.imageContentWrapper);
+      } else {
+        this.imageContentWrapper.style.display = "inline";
+        this.wrapper.appendChild(this.imageContentWrapper);
+      }
+    }
+
+
     this.contentWrapper = document.createElement("div");
     this.contentWrapper.className = "couple-days-content";
     this.wrapper.appendChild(this.contentWrapper);
+
+
+    if (this.config.showExtraDates && this.config.extraDates.length > 0) {
+      this.extraContentWrapper = document.createElement("div");
+      this.extraContentWrapper.className = "couple-days-extra-content";
+      this.wrapper.appendChild(this.extraContentWrapper);
+      this.updateExtraContent();
+    }
+
+
     this.updateContent();
     this.updateDom();
+
     setInterval(() => {
       this.currentIndex = (this.currentIndex + 1) % this.views.length;
       this.currentView = this.views[this.currentIndex];
       this.updateContent();
+      if (this.config.showExtraDates && this.config.extraDates.length > 0) {
+        this.updateExtraContent();
+      }
     }, this.config.transitionDuration);
   },
 
@@ -60,8 +102,8 @@ Module.register("MMM-CoupleDays", {
     return `${this.config.name1} ${this.translate("and")} ${this.config.name2}`;
   },
 
-  getDuration (unit) {
-    return this.endDate.diff(this.startDate, unit);
+  getDuration (unit, startDate = this.startDate) {
+    return this.endDate.diff(startDate, unit);
   },
 
   formatDuration (duration, unit) {
@@ -74,6 +116,10 @@ Module.register("MMM-CoupleDays", {
 
   updateContent () {
     this.contentWrapper.innerHTML = this.getFormattedDuration();
+  },
+
+  updateExtraContent () {
+    this.extraContentWrapper.innerHTML = this.getExtraFormattedDuration();
   },
 
   getFormattedDuration () {
@@ -91,14 +137,44 @@ Module.register("MMM-CoupleDays", {
     }
   },
 
+  getExtraFormattedDuration () {
+    let view = "";
 
-  formatYears () {
-    const years = Math.floor(this.getDuration("years"));
-    const months = Math.floor(this.getDuration("months") % 12);
+    let viewIndex = this.currentView.key;
+    if (this.config.ExtraDatesViewMode && this.config.ExtraDatesViewMode !== "loop") {
+      viewIndex = this.config.ExtraDatesViewMode;
+    }
+
+    this.config.extraDates.forEach((extraDate) => {
+      let formatDuration = "";
+
+      if (viewIndex === "years") {
+        formatDuration = this.formatYears(moment(extraDate.date));
+      } else if (viewIndex === "total") {
+        formatDuration = this.formatTotal(moment(extraDate.date));
+      } else {
+        formatDuration = this.formatDuration(this.getDuration(viewIndex, moment(extraDate.date)), viewIndex);
+      }
+
+      view += `
+        <div class="extra-content-main">
+          ${formatDuration}
+          <div class="extra-label">${` ${extraDate.label}`}</div>
+        </div>
+      `;
+    });
+
+    return view;
+  },
+
+
+  formatYears (startDate) {
+    const years = Math.floor(this.getDuration("years"), startDate);
+    const months = Math.floor(this.getDuration("months", startDate) % 12);
 
     // Berechne die tatsächliche Anzahl der Tage korrekt
-    const totalDays = this.getDuration("days");
-    const yearMonthDays = (years * 365) + (months * 30);
+    const totalDays = this.getDuration("days", startDate);
+    const yearMonthDays = years * 365 + months * 30;
     const days = totalDays - yearMonthDays;
     if (years === 0) {
       return `${this.formatDuration(months, this.translate("months"))} ${this.formatDuration(days, this.translate("days"))}`;
@@ -112,13 +188,13 @@ Module.register("MMM-CoupleDays", {
     return formattedYears;
   },
 
-  formatTotal () {
-    const years = Math.floor(this.getDuration("years"));
-    const months = Math.floor(this.getDuration("months") % 12);
+  formatTotal (startDate) {
+    const years = Math.floor(this.getDuration("years", startDate));
+    const months = Math.floor(this.getDuration("months", startDate) % 12);
 
     // Berechne die tatsächliche Anzahl der Tage korrekt
-    const totalDays = this.getDuration("days");
-    const yearMonthDays = (years * 365) + (months * 30);
+    const totalDays = this.getDuration("days", startDate);
+    const yearMonthDays = years * 365 + months * 30;
     const days = totalDays - yearMonthDays;
     const formattedYears = this.formatDuration(years, this.translate("years"));
     const formattedMonths = this.formatDuration(months, this.translate("months"));
